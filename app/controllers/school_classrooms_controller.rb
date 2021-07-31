370c613ad257fbfc0dc12f5da4bb1a544be3e725
young <purpleice9765@msn.com>
2021-08-01 00:56:58 +0800

    classrom CRUD

---
+++ app/controllers/school_classrooms_controller.rb
@@ -0,0 +1,65 @@
+class SchoolClassroomsController < ApplicationController
+
+    before_action :find_school
+
+    def index
+        @classrooms = @school.classrooms
+    end
+
+    def show
+        @classroom = @school.classrooms.find( params[:id] )
+    end
+
+    def new
+        @classroom = @school.classrooms.build
+    end
+
+    def create
+        @classroom = @school.classrooms.build( classroom_params )
+        if @classroom.save
+            redirect_to school_classrooms_url( @school )
+        else
+            render :action => :new
+        end
+    end
+
+    def edit
+        @classroom = @school.classrooms.find( params[:id] )
+    end
+
+    def update
+        @classroom = @school.classrooms.find( params[:id] )
+
+        if @classroom.update( classroom_params )
+            redirect_to school_classrooms_url( @school )
+        else
+            render :action => :edit
+        end
+
+    end
+
+    def destroy
+        @classroom = @school.classrooms.find( params[:id] )
+        @classroom.destroy
+
+        redirect_to school_classrooms_url( @school )
+    end
+
+    protected
+
+    def find_school
+        @school = School.find( params[:school_id] )
+    end
+
+    def classroom_params
+        params.require(:classroom).permit(
+            :name, 
+            :description,
+            :city,
+            :school_name,
+            :school_type,
+            :grade
+        )
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :schools do
     resources :school_staffs, :controller => 'school_school_staffs'
+    resources :classrooms, :controller => 'school_classrooms'
   end
 
 
