99b5e05686a3af14d4113d0a7d70f058b69e594d
young <purpleice9765@msn.com>
2021-07-31 22:01:12 +0800

    paginate

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,7 +2,7 @@ class SchoolsController < ApplicationController
     before_action :set_school, :only => [ :show, :edit, :update, :destroy]
 
     def index
-        @schools = School.all
+        @schools = School.page(params[:page]).per(5)
     end
 
     def new
