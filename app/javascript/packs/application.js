00d9ff357684ef52a74fd4a0fe67906e0d7cda17
young <purpleice9765@msn.com>
2021-08-05 04:29:39 +0800

    bootstrap home index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,28 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap";
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'style'
 import 'jquery'
 import 'popper.js'
-import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all"
+import 'bootstrap/dist/js/bootstrap'
+
+const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function () {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
