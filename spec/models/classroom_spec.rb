278da3e56b68fadc50ec628300833e2017b526a7
young <purpleice9765@msn.com>
2021-09-07 21:00:58 +0800

    rspec classroom

---
+++ spec/models/classroom_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Classroom, type: :model do
+	describe 'associations' do
+        it { should belong_to(:school) }
+		it { should have_many(:classroom_teacher_ships) }
+		it { should have_many(:teachers).through(:classroom_teacher_ships) }
+        it { should have_many(:classroom_course_ships) }
+		it { should have_many(:courses).through(:classroom_course_ships) }
+  end
+end
